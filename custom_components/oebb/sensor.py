bc7aa4dc56cc6e4ecbe5c245497cd785589834bf
lollopod <campioni.lorenzo@gmal.com>
2022-11-15 09:56:14 +0000

    Some tests.

--- custom_components/oebb/sensor.py
+++ custom_components/oebb/sensor.py
@@ -3,7 +3,7 @@ A integration that allows you to get information about next departure from speci
 For more details about this component, please refer to the documentation at
 https://github.com/tofuSCHNITZEL/home-assistant-wienerlinien
 """
-from datetime import timedelta
+from datetime import datetime, timedelta, time
 import json
 import logging
 
@@ -17,7 +17,11 @@ from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
-
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
 
 CONF_L = "L"
 CONF_EVAID = "evaId"
@@ -75,47 +79,131 @@ async def async_setup_platform(hass, config, add_devices_callback, discovery_inf
 
     api = OebbAPI(async_create_clientsession(hass), hass.loop, params)
     data = await api.get_json()
+    _LOGGER.debug(len(data["journey"]))
 
+
+`   coordinator = OebbAPI(hass, my_api)
+
+    devices.append(api)
     for index, journey in enumerate(data["journey"]):
-
         try:
-            name = "journey_" + index
+            name = "oebb_journey_" + str(index)
         except Exception:
             raise PlatformNotReady()
-        devices.append(OebbSensor(api, name, index))
+        devices.append(OebbSensor(api, name, index, params["evaId"]))
+
     add_devices_callback(devices, True)
+
+
+class OebbAPI(DataUpdateCoordinator):
+    """Call API."""
+
+    def __init__(self, session, loop, params):
+        """Initialize."""
+        self.params = params
+        self.loop = loop
+        self.session = session
+        self._name = "oebb_api"
+        self._state = None
+        self.data = {}
+        self.attributes = {}
+
+        req = PreparedRequest()
+        req.prepare_url(BASE_URL, self.params)
+        self.url = req.url
+        self._attr_unique_id = self.url
+
+    async def get_json(self):
+        """Get json from API endpoint."""
+        value = None
+
+        _LOGGER.debug("Inside get JSON")
+
+        try:
+            async with async_timeout.timeout(10):
+
+                response = await self.session.get(self.url)
+
+        except Exception:
+            pass
+        string = str(response.content._buffer[0]).replace("\\n", "")[16:-1]
+
+        value = json.loads(string)
+
+        return value
+
+    async def async_update(self):
+        """Update data for the entire API."""
+        try:
+            self.data = await self.get_json()
+            _LOGGER.debug(self.data)
+            if self.data is None:
+                return
+            # data = data.get("data", {})
+        except:
+            _LOGGER.debug("Could not get new state")
+            return
+
+    @property
+    def name(self):
+        """Return name."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return state."""
+        return self._state
+
+    @property
+    def icon(self):
+        """Return icon."""
+        return "mdi:server-network"
+
+    @property
+    def extra_state_attributes(self):
+        """Return attributes."""
+        return self.attributes
 
 
 class OebbSensor(Entity):
     """OebbSensor."""
 
-    def __init__(self, api, name, index):
+    def __init__(self, api: OebbAPI, name, index, evaId):
         """Initialize."""
         self.api = api
         self.index = index
+        self.formatted_index = f"{self.index:02}"
         self._name = name
         self._state = None
         self.attributes = {}
+        self._attr_unique_id = str(evaId) + "_" + str(index)
 
     async def async_update(self):
         """Update data."""
+
         try:
-            data = await self.api.get_json()
-            _LOGGER.debug(data)
-            if data is None:
+            # await self.api.async_update()
+            data = self.api.attributes
+            _LOGGER.debug(self.api.data)
+            if self.api.data is None:
                 return
-            data = data.get("data", {})
+            # data = data.get("data", {})
         except:
             _LOGGER.debug("Could not get new state")
             return
 
-        if data is None:
-            return
         try:
+            now = datetime.now()
+            timestamp_string = (
+                now.strftime("%m/%d/%Y")
+                + " "
+                + data["journey"][self.formatted_index]["ti"]
+            )
+            self._state = time.strptime(timestamp_string)
             self.attributes = {
-                "startTime": data["journey"][self.index]["ti"],
-                "lastStop": data["journey"][self.index]["LastStop"],
-                "line": data["journey"][self.index]["pr"],
+                "startTime": data["journey"][self.formatted_index]["ti"],
+                "lastStop": data["journey"][self.formatted_index]["LastStop"],
+                "line": data["journey"][self.formatted_index]["pr"],
             }
 
         except Exception:
@@ -129,10 +217,7 @@ class OebbSensor(Entity):
     @property
     def state(self):
         """Return state."""
-        if self._state is None:
-            return self._state
-        else:
-            return f"{self._state[:-2]}:{self._state[26:]}"
+        return self._state
 
     @property
     def icon(self):
@@ -148,35 +233,3 @@ class OebbSensor(Entity):
     def device_class(self):
         """Return device_class."""
         return "timestamp"
-
-
-class OebbAPI:
-    """Call API."""
-
-    def __init__(self, session, loop, params):
-        """Initialize."""
-        self.params = params
-        self.loop = loop
-        self.session = session
-
-    async def get_json(self):
-        """Get json from API endpoint."""
-        value = None
-
-        _LOGGER.debug("Inside get JSON")
-        req = PreparedRequest()
-        req.prepare_url(BASE_URL, self.params)
-        try:
-            async with async_timeout.timeout(10):
-
-                response = await self.session.get(req.url)
-
-            # value = json.loads(response_string[14:])
-
-        except Exception:
-            pass
-        string = str(response.content._buffer[0]).replace("\\n", "")[16:-1]
-
-        value = json.loads(string)
-
-        return value
